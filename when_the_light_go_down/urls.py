762493cc9b7246cb3ba6fddd46bc22bddbdd5ec6
Unknown <Catablor.prout@gmail.com>
2018-07-22 18:44:29 +0200

    correction des parenthèses

--- when_the_light_go_down/urls.py
+++ when_the_light_go_down/urls.py
@@ -20,7 +20,8 @@ from when_the_light_go_down import views
 urlpatterns = [
     path('fiche_hash', views.fiche_hash, name='fiche_hash'),
     path('fiche', views.fiche_render, name='fiche_render'),
-    path('tableaux_prix', views.tableau_prix, name='tableau_prix')
-],
-    path('tableaux_salaire', views.tableau_salaire, name='tableau_salaire')
+    path('tableau_prix', views.tableau_prix, name='tableau_prix'),
+	path('tableau_salaire', views.tableau_salaire, name='tableau_salaire')
 ]
+
+    
